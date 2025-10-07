d22703acb89578fc69f98571be3bab1774a41b2d
xiao-xuyu <2543538397@qq.com>
2025-10-07 18:00:06 +0800

    update

--- syntax/dependency-injection/src/main/java/com/example/demo/RootPerson.java
+++ syntax/dependency-injection/src/main/java/com/example/demo/RootPerson.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Person {
-    private Tool tool;
+public class RootPerson {
+    @Autowired
+    private RootTool tool;
     public void UseTool() {
         tool.Work();
     }
--- syntax/dependency-injection/src/main/java/com/example/demo/RootTool.java
+++ syntax/dependency-injection/src/main/java/com/example/demo/RootTool.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Tool {
+public class RootTool {
     public void Work() {
         System.out.println("working");
     }
