aa81e48fa9ccbcb41d6689a38b3d6871b14903b8
Terry Su 苏溪云 <2684759855@qq.com>
2025-10-15 10:58:52 +0800

    update

---
+++ syntax/java-syntax/src/main/java/tmp/Tmp.java
@@ -0,0 +1,16 @@
+package tmp;
+
+class Counter {
+    static int count = 1;
+    public void addCount() {
+        count++;
+    }
+}
+
+public class Tmp {
+    public static void main(String[] args) {
+        Counter c = new Counter();
+        c.addCount();
+        System.out.println(c.count);
+    }
+}
