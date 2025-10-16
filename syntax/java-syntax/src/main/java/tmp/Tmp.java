695143a7061952f0ede6075c8f58c48094989ec5
Terry Su 苏溪云 <2684759855@qq.com>
2025-10-16 14:14:22 +0800

    update

--- syntax/java-syntax/src/main/java/tmp/Tmp.java
+++ syntax/java-syntax/src/main/java/tmp/Tmp.java
@@ -1,16 +1,21 @@
 package tmp;
 
-class Counter {
-    static int count = 1;
-    public void addCount() {
-        count++;
-    }
-}
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
+
+@SpringBootApplication
 public class Tmp {
+    @Value("${spring.application.name}")
+    private String appName;
+
+
     public static void main(String[] args) {
-        Counter c = new Counter();
-        c.addCount();
-        System.out.println(c.count);
+        ConfigurableApplicationContext context = SpringApplication.run(Tmp.class, args); // 运行
+        Tmp app = context.getBean(Tmp.class);
+        System.out.println("App Name = " + app.appName);
+//        System.out.println("appName" + appName);
     }
 }
