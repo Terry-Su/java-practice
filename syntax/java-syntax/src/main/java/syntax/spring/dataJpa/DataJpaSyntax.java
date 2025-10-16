955871bfca775acb042e14f0eb1a9e6472721fec
Terry Su 苏溪云 <2684759855@qq.com>
2025-10-16 17:16:05 +0800

    update

---
+++ syntax/java-syntax/src/main/java/syntax/spring/dataJpa/DataJpaSyntax.java
@@ -0,0 +1,11 @@
+package syntax.spring.dataJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaSyntax {
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaSyntax.class, args);
+    }
+}
---
+++ syntax/java-syntax/src/main/java/syntax/spring/dataJpa/UserRepository.java
@@ -0,0 +1,7 @@
+package syntax.spring.dataJpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByName(String name);
+}
