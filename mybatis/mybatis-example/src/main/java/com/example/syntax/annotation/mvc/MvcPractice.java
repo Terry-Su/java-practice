a59a38942fbbef4f1d4128d8712fd5c97a923d88
Terry Su 苏溪云 <2684759855@qq.com>
2025-10-19 15:32:53 +0800

    update

--- mybatis/mybatis-example/src/main/java/com/example/syntax/annotation/mvc/MvcPractice.java
+++
@@ -1,5 +0,0 @@
-package com.example.syntax.annotation.mvc;
-
-public class MvcPractice {
-    
-}
---
+++ mybatis/mybatis-example/src/main/java/mapper/EmployeeMapper.java
@@ -0,0 +1,7 @@
+package mapper;
+
+import demo.Employee;
+
+public interface EmployeeMapper {
+    Employee selectEmployee(Integer id);
+}
